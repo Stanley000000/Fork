b1ed0e0b7e5c47c84c3fce183a41590967f73f3b
Stanley000000 <davidjosephxx5@gmail.com>
2024-04-01 00:10:05 +0100

    first commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://slerffork.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://slerffork.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://slerffork.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://slerffork.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://slerffork.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://slerffork.com/index.php?rest_route=/" />
+			</apis>
+	</service>
+</rsd>
+	
